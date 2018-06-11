385982dc3382d624c2dffdeb9e1e23d6c80c9ca9
Eponick <mikehuntwife@gmail.com>
2018-06-11 19:06:45 -0400

    Check valid word/force fail

---
+++ test_hangman.rb
@@ -0,0 +1,9 @@
+ISTESTING = true
+require "./src/hangman.rb"
+require "test/unit"
+
+class Test_hangman_game < Test::Unit::TestCase
+    def test_check_valid_word
+        assert_true(check_valid_word("hello"))
+    end
+end
