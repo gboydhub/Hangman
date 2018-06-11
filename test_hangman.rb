6e3abd1131d2a72c46fe9737cc7cec981103ae5d
Eponick <mikehuntwife@gmail.com>
2018-06-11 19:32:35 -0400

    check_valid_word/fail number string

--- test_hangman.rb
+++ test_hangman.rb
@@ -6,5 +6,6 @@ class TestHangmanGame < Test::Unit::TestCase
     def test_check_valid_word
         assert_true(check_valid_word('hello'))
         assert_false(check_valid_word(2))
+        assert_false(check_valid_word('h3i'))
     end
 end
