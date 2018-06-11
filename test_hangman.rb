e8d3e244ab161eb74d61814a43d44f528fd11820
Eponick <mikehuntwife@gmail.com>
2018-06-11 19:13:17 -0400

    check valid word/fail integer check

--- test_hangman.rb
+++ test_hangman.rb
@@ -5,5 +5,6 @@ require "test/unit"
 class Test_hangman_game < Test::Unit::TestCase
     def test_check_valid_word
         assert_true(check_valid_word("hello"))
+        assert_false(check_valid_word(2))
     end
 end
