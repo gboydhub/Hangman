637fac38e2d1bd63a6105ed04aaf4e2a4e8f78f4
Eponick <mikehuntwife@gmail.com>
2018-06-11 20:47:43 -0400

    check_valid_guess/string of characters fail

--- test_hangman.rb
+++ test_hangman.rb
@@ -16,5 +16,6 @@ class TestHangmanGame < Test::Unit::TestCase
     def test_check_valid_guess
         assert_true(check_valid_guess?('a'))
         assert_false(check_valid_guess?('1'))
+        assert_false(check_valid_guess?('hi'))
     end
 end
