179a203feb2969f70219629a71ce23d9686b0c06
Eponick <mikehuntwife@gmail.com>
2018-06-11 20:36:39 -0400

    check valid word/pass incorrect string

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,8 @@
-def check_valid_word(str)
-    str.is_a?(String)
+def check_valid_word?(str)
+    unless str.is_a?(String) then return false end
+    if str[/[a-zA-Z]+/] == str
+        return true
+    end
+
+    return false
 end
--- test_hangman.rb
+++ test_hangman.rb
@@ -4,8 +4,8 @@ require 'test/unit'
 
 class TestHangmanGame < Test::Unit::TestCase
     def test_check_valid_word
-        assert_true(check_valid_word('hello'))
-        assert_false(check_valid_word(2))
-        assert_false(check_valid_word('h3i'))
+        assert_true(check_valid_word?('hello'))
+        assert_false(check_valid_word?(2))
+        assert_false(check_valid_word?('h3i'))
     end
 end
