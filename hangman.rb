f901f801593c802aa8b542f5d9cd30d6ec195595
Eponick <mikehuntwife@gmail.com>
2018-06-11 22:16:13 -0400

    Semi-playable version of hangman

--- hangman.rb
+++ hangman.rb
@@ -1,4 +1,5 @@
 require './tested_functions.rb'
+require 'io/console'
 
 puts "Welcome to Hangman"
 
@@ -8,6 +9,50 @@ while gamerunning do
     while !check_valid_word?(userinput) do
         print "Player 1[Executioner], choose a word: "
         userinput = gets.chomp
+    end
+
+    gameboard = userinput.split('')
+    answerboard = userinput.split('')
+    answerboard.each_with_index do |v, i|
+        gameboard[i] = '_'
+    end
+
+    stickmanprogress = [' ', ' ', ' ', ' ', ' ', ' ']
+    numberwrong = 0
+    numbercorrect = 0
+    
+    playloop = true
+    while playloop do
+        system 'clear' or system 'cls'
+        puts <<-GAME_BOARD
+            _____________
+            /  ___________\\
+            | /      |                          
+            ||       O                      #{gameboard}
+            ||      /|\\
+            ||      / \\
+            ||===========
+            ||
+            ||
+        ===========
+        GAME_BOARD
+
+        input = STDIN.getch
+        wronganswer = false
+        unless check_valid_guess?(input); wronganswer = true end
+        if !wronganswer && answerboard.include?(input)
+            answerboard.each_with_index do |c, i|
+                if c == input; gameboard[i] = c.upcase end
+                numbercorrect += 1
+            end
+        end
+
+        if wronganswer
+            numberwrong += 1
+        end
+        if numberwrong > 6
+            playloop = false
+        end
     end
 
     system 'clear' or system 'cls'
