85412648900fd954f0085387aea554731ca08e0b
Eponick <mikehuntwife@gmail.com>
2018-06-11 21:23:55 -0400

    Created game loop, get word from p1

--- hangman.rb
+++ hangman.rb
@@ -14,3 +14,14 @@ end
 if defined?(ISTESTING); exit end
 
 puts "Welcome to Hangman"
+
+gamerunning = true
+while gamerunning do
+    userinput = 0
+    while !check_valid_word?(userinput) do
+        print "Player 1[Executioner], choose a word: "
+        userinput = gets.chomp
+    end
+
+    puts "Selected valid word!"
+end
